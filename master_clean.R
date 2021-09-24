5713fd5972a1d0578f452366b5ba9525875dd842
blainmorin <morin.42@osu.edu>
2021-09-24 16:22:16 -0400

    changed the file location

--- master_clean.R
+++ master_clean.R
@@ -3,9 +3,15 @@
 
 
 ### Start by calling data from the drive
-require(googlesheets4) == T || install.packages("googlesheets4")
-library(googlesheets4)
-law.df = read_sheet("https://docs.google.com/spreadsheets/d/1Zz50a6TH-Y56Sv7h1JVlmoGnd8llkCv4qDNX9Qs4YI0/edit#gid=0")
+# require(googlesheets4) == T || install.packages("googlesheets4")
+# library(googlesheets4)
+# law.df = read_sheet("https://docs.google.com/spreadsheets/d/1Zz50a6TH-Y56Sv7h1JVlmoGnd8llkCv4qDNX9Qs4YI0/edit#gid=0")
+
+
+require(readr) == T || install.packages("readr")
+library(readr)
+law.df = read_csv("cases_coded_prelim_clean1.csv")
+
 
 ### Put other packages here
 require(tidyverse) == T || install.packages("tidyverse")
