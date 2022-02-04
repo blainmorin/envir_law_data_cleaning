0ce782125f4e47c31402a8af8c3614ec71787765
Casey R <caseyr@MacBook-Pro-2.local>
2022-02-03 22:24:25 -0500

    cleaned deference column, coded for circuits from district court column, 4th and 5th circuits having issues

---
+++ casey/casey_deference.R
@@ -0,0 +1,43 @@
+#packages
+
+library(tidyverse)
+library(stringr)
+
+#load data
+
+data <- read_csv("cases_coded_prelim_clean1.csv")
+
+#clean typos
+
+data_deference <- data %>%
+  mutate(
+    `deference` = str_replace_all(`deference`,"Yes","yes"),
+    `deference` = str_replace_all(`deference`,"scientific expertise","no"),
+    `deference` = str_replace_all(`deference`,"NULL","no"),
+    `deference` = str_replace_all(`deference`,"None","no"),
+    `deference` = str_replace_all(`deference`,"none","no"),
+    `deference` = str_replace_all(`deference`,"No","no"),
+    `deference` = str_replace_all(`deference`,"no","no"),
+    `deference` = str_replace_all(`deference`,"n","no"),
+    `deference` = str_replace_all(`deference`,"mixed","yes"),
+    `deference` = str_replace_all(`deference`,"agency expertise","no"),
+    `deference` = str_replace_all(`deference`,"12","yes"),
+    `deference` = str_replace_all(`deference`,"1","yes"),
+    `deference` = str_replace_all(`deference`,"0","no"),
+    `deference` = str_replace_all(`deference`,"agenocy expertise","no"),
+    `deference` = str_replace_all(`deference`,"noo","no")
+  )
+
+#deference is clean
+
+#create final dataframe and rename column for binding data
+
+deference_c <- data_deference %>%
+  select(
+    `ID`, `deference`
+  ) %>%
+  rename(
+    `deference_c` = `deference`)
+    
+    
+    
